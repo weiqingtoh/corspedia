70887744ce6e2bc6a72951dec5820450b676d4b8
Weiqing <weiqingtoh@gmail.com>
2013-06-15 02:17:32 +0800

    Updated data file and query logic. Fixed bugs
    To find more bugs. Next step to format output to JSON.

--- query.py
+++ query.py
@@ -16,7 +16,7 @@ facultyList = {"ART":"ARTS & SOCIAL SCIENCES",
                "MED":"YONG LOO LIN SCHOOL OF MEDICINE",
                "YST":"YONG SIEW TOH CONSERVATORY OF MUSIC"}
 
-def extract(modCode, faculty, accType, newStudent):
+def extract(modCode, faculty, accType, newStu):
     output = []
     out = []
     faculties = []
@@ -25,32 +25,48 @@ def extract(modCode, faculty, accType, newStudent):
         for row in modData:
             #Extract Records for Account and ModCode
             if row[0] == modCode:
-                if accType == 'p' and row[9]:
+                if accType == 'p' and row[9] == '1':
                     output.append(row)
-                elif accType == 'g' and (row[10] == '1' or row[11][0] == '3'):
+                elif accType == 'g' and row[10] == '1':
                     output.append(row)
                     
     #Correct records for faculty, bidrounds
-    for row in output:
-        if newStudent == "0" and row[8] == '0' and row[9] == '1' and row[10] == '1':
-            out.append(row)
-        elif row[11] in ["3A","3B","3C"]:
-            out.append(row)
-        elif facultyList[faculty] == row[7]:
-            if newStudent == "0":
-                if row[8] == '0' or row[8] == '2':
-                    out.append(row)
-            elif newStudent == "1":
-                if row[8] == '1' or row[8] == '2':
-                    out.append(row)
-
-    ##Form output data structure. Remember to remove some duplicate entries.
-    ##Eg. FIN3130 BIZ p 0 query would return repeats. 
+    #if modCode is SS or GEM format and return output
+    if modCode[0:3] in ('SSA','SSB','SSD','SSS','GEK','GEM'):
+        return output
                     
+    for row in output:
+        #Filter by Rounds - Round 1A,1B,1C
+        if row[11][0] == '1':
+            if newStu == '0' and row[8] != '1':
+                if row[7] == facultyList[faculty]:
+                    out.append(row)
+        #Round 2A, 2B, 2C
+        elif row[11][0] == '2':
+            if row[11] == '2C':
+                out.append(row)
+            elif newStu == '0' and row[8] != '1':
+                if facultyList[faculty] == row[7]:
+                    out.append(row)
+            elif newStu == '1' and row[8] != '0':
+                if facultyList[faculty] == row[7]:
+                    out.append(row)
+        #Round 3A, 3B, 3C
+        else:
+            out.append(row)      
     return out
 
-for row in extract("ACC1002X","SCI","p","0"):
-    print row
 
-## Rules. Round 3A and 3B always in.
-## Rules. row[8]: NewStu - 2 is for both new and not new, 1 for new, 0 for old.
+def outformat(list):
+    out = []
+    #Count number of lecture groups
+    #Create output data structure
+    #Look up nec information
+    
+    for row in list:
+        out.append(row)
+    return out
+
+##
+##for row in extract("GEM2900","BIZ","g","0"):
+##    print row
