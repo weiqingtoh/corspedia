1e6f5cdba600cf088d83c4283efd3286c4a01155
Weiqing <weiqingtoh@gmail.com>
2013-06-17 00:18:47 +0800

    Added module names + Format query to JSON output.

--- query.py
+++ query.py
@@ -33,7 +33,7 @@ def extract(modCode, faculty, accType, newStu):
     #Correct records for faculty, bidrounds
     #if modCode is SS or GEM format and return output
     if modCode[0:3] in ('SSA','SSB','SSD','SSS','GEK','GEM'):
-        return output
+        return outformat(output, modCode)
                     
     for row in output:
         #Filter by Rounds - Round 1A,1B,1C
@@ -54,19 +54,52 @@ def extract(modCode, faculty, accType, newStu):
         #Round 3A, 3B, 3C
         else:
             out.append(row)      
-    return out
+    return outformat(out,modCode)
+
+#Can remove modCode and seek modCode within data
+def outformat(bidInfo, modCode):
+    data = {}
+    data['Title'] = ""
+    data['Module'] = modCode
+    with open('modName.csv','rb') as csvfile:
+        modData = csv.reader(csvfile)
+        for row in modData:
+            if row[0] == modCode:
+                data['Title']= row[1]
+            
+    #Create Bidhstory
+    bidHist = {}
+    for year in ['2008','2009','2010','2011','2012']:
+        for sem in ['1','2']:
+            aySem = year + 'S' + sem
+            bidHist[aySem] = {}
+            lectGrp, tempList = [], []
+
+            #Extract Relevant Rows to Display in AY and Sem
+            for row in bidInfo:
+                if row[12] == year and row[13] == sem:
+                    tempList.append(row)
+                    if row[1] not in lectGrp:
+                        lectGrp.append(row[1])
+
+            #Count number of Lecture Groups, Create Dictionary Output
+            numLect = len(lectGrp)
+            lectGrp.sort()
+            for i in range(0,numLect):
+                letter = chr(ord('A')+ i)
+                bidHist[aySem][letter] = {}
+                for bidRd in ['1A','1B','1C','2A','2B','2C','3A','3B','3C']:
+                    bidHist[aySem][letter][bidRd] = [0,0,0,0,0]
+
+            #Input each entry
+            for entry in tempList:
+                letter = chr(ord('A')+lectGrp.index(entry[1]))
+                for i in range(0,5):
+                    bidHist[aySem][letter][entry[11]][i]=int(entry[i+2])
+    data['BidHistory'] = bidHist
+    return data
 
 
-def outformat(list):
-    out = []
-    #Count number of lecture groups
-    #Create output data structure
-    #Look up nec information
-    
-    for row in list:
-        out.append(row)
-    return out
 
-##
-##for row in extract("GEM2900","BIZ","g","0"):
+print extract("GEM2900","BIZ","g","0")
 ##    print row
