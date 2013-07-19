0c146f68cca254d5b05ccf5d7b46fec3b4c06d61
Yang Shun <tay.yang.shun@gmail.com>
2013-07-19 21:39:04 +0800

    added jquery ui and data is now displayed by year

--- js/results-controller.js
+++ js/results-controller.js
@@ -24,20 +24,9 @@ function resultsController($scope, $http) {
 	$http.get(constructQueryURL()).success(function(res) {
 		$scope.data = res;
 		console.log($scope.data);
-		var bid_history = [];
-		for (var sem in $scope.data.BidHistory) {
-			for (var lecture_grp in $scope.data.BidHistory[sem]) {
-				for (var bidding_round in $scope.data.BidHistory[sem][lecture_grp]) {
-					var obj = { sem: sem, 
-								bidding_round: bidding_round, 
-								lecture_grp: lecture_grp, 
-								value: $scope.data.BidHistory[sem][lecture_grp][bidding_round]};
-					bid_history.push(obj);
-				}
-			}
-		}
-		console.log(bid_history);
-		$scope.data.bid_history = bid_history;
+		setTimeout(function() {
+			$("#bidding-history-table").tabs();
+		}, 0);
 	}).error(function(res) {
 		alert('An error occurred.');
 	});
