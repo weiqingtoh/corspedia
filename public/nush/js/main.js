219269ef9aaba30b3256ea8b23fb58b1d0162ed2
Yang Shun <tay.yang.shun@gmail.com>
2014-01-12 16:33:04 +0800

    temp nush stuff

--- public/nush/js/main.js
+++ public/nush/js/main.js
@@ -2,53 +2,78 @@ var YES = 'YES';
 var NO = 'NO'
 var SOMEWHAT = 'SW';
 
+var mandatory_questions = [{
+	question: 'createdAt',
+	order: 0.1
+},
+{
+	question: 'Cluster mentor',
+	order: 0.2
+},
+{
+	question: 'Room',
+	order: 0.3
+},
+]
+
 var individual_questions = 
 [{
 	question: 'Floor is clutter free?',
-	options: [YES, SOMEWHAT, NO]
+	options: [YES, SOMEWHAT, NO],
+	order: 1.1
 },
 {
 	question: 'Table is dust free?',
-	options: [YES, SOMEWHAT, NO]
+	options: [YES, SOMEWHAT, NO],
+	order: 1.2
 },
 {
 	question: 'Rubbish bin is emptied?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 1.3
 },
 {
 	question: 'Bedsheet on mattress?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 1.4
 },
 {
 	question: 'Bed made and free of clutter?',
-	options: [YES, SOMEWHAT, NO]
+	options: [YES, SOMEWHAT, NO],
+	order: 1.5
 },
 {
 	question: 'Clean clothes in cupboard?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 1.6
 },
 {
 	question: 'Dirty laundry properly organised?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 1.7
 }
 ];
 
 var room_questions = 
 [{
 	question: 'Floor is dust free?',
-	options: [YES, SOMEWHAT, NO]
+	options: [YES, SOMEWHAT, NO],
+	order: 2.1
 },
 {
 	question: 'Door can be opened fully?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 2.2
 },
 {
 	question: 'Shoes on the shoe rack?',
-	options: [YES, SOMEWHAT, NO]
+	options: [YES, SOMEWHAT, NO],
+	order: 2.3
 },
 {
 	question: 'Corridor free of clutter?',
-	options: [YES, NO]
+	options: [YES, NO],
+	order: 2.4
 }
 ];
 
@@ -57,22 +82,26 @@ var common_area_questions =
 	section: 'Shower Stall',
 	questions: [{
 		question: 'Shower stall is clean?',
-		options: [YES, SOMEWHAT, NO]
+		options: [YES, SOMEWHAT, NO],
+		order: 3.1
 	}]
 },
 {
 	section: 'Toilet Cubicle',
 	questions: [{
 		question: 'Toilet bowl is free from stains?',
-		options: [YES, SOMEWHAT, NO]
+		options: [YES, SOMEWHAT, NO],
+		order: 3.2
 	},
 	{
 		question: 'Toilet seat is clean?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.3
 	},
 	{
 		question: 'Free of toilet roll cores?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.4
 	}
 	]
 },
@@ -80,24 +109,33 @@ var common_area_questions =
 	section: 'Wash Area',
 	questions: [{
 		question: 'Pole rack in wash area?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.5
 	},
 	{
 		question: 'Shelf is free of stains?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.6
 	},
 	{
 		question: 'Sinks are clean?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.7
 	},
 	{
 		question: 'Wash area floor is free of rubbish?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.8
 	},
 	{
 		question: 'Wash area floor is free of clutter?',
-		options: [YES, NO]
+		options: [YES, NO],
+		order: 3.9
 	}
 	]
 }
-]
+];
+
+function sanitizeQuestion(question) {
+	return question.replace(/ /g, "_").replace(/\?/g, "");
+}
--- public/nush/js/questions-controller.js
+++ public/nush/js/questions-controller.js
@@ -1,6 +1,40 @@
 function QuestionsController($scope) {
-	
 
+	// Loading of preferences
+	if (localStorage['name']) {
+		$scope.name = localStorage['name'];
+	}
+
+	// Saving of preferences
+	if (localStorage['cluster']) {
+		$scope.cluster = localStorage['cluster'];
+	}
+
+	$scope.savePreferences = function() {
+		localStorage['name'] = $scope.name;
+		localStorage['cluster'] = $scope.cluster;
+	}
+
+	// Auto updating of room numbers
+	$scope.changeRoom = function(num) {
+		if (num == 'first') {
+			var first_room = parseInt($scope.first_room);
+			if (first_room % 2 == 1) {
+				$scope.second_room = (first_room+1).toString();
+			} else {
+				$scope.second_room = (first_room-1).toString();
+			}
+		} else {
+			var second_room = parseInt($scope.second_room);
+			if (second_room % 2 == 1) {
+				$scope.first_room = (second_room+1).toString();
+			} else {
+				$scope.first_room = (second_room-1).toString();
+			}
+		}
+	}
+
+	// Questions
 	$scope.individual_questions = angular.copy(individual_questions);
 	$scope.individual_responses = [{}, {}, {}, {}];
 
@@ -8,10 +42,6 @@ function QuestionsController($scope) {
 		for (var j = 0; j < 4; j++) {
 			$scope.individual_responses[j][sanitizeQuestion($scope.individual_questions[i].question)] = "";
 		}
-	}
-
-	function sanitizeQuestion(question) {
-		return question.replace(/ /g, "_").replace(/\?/g, "");
 	}
 
 	$scope.editIndividualQuestionResponse = function(index, question, response) {
@@ -70,22 +100,96 @@ function QuestionsController($scope) {
 	}
 
 
-	$scope.submit = function() {
+	$scope.submit = function() {	
+		if (!confirm('Are you sure you want to submit the responses?')) { return; }
 
-        var ResObject = Parse.Object.extend("ResponseObject");
-        var resObject = new ResObject();
+        var CleanlinessObject = Parse.Object.extend("CleanlinessObject");
+        var objects_to_submit = [];
+        
+        var responses_to_submit = [];
 
-        resObject.save(
-        	$scope.responses, {
-          	success: function(data) {
-			    // Execute any logic that should take place after the object is saved.
-			    alert('Response Submitted!');
-			  },
-			  error: function(data, error) {
-			    // Execute any logic that should take place if the save fails.
-			    // error is a Parse.Error with an error code and description.
-			    alert('Failed to submit response: ' + error.description);
-			  }
-        });
+        function formatRoom(num) {
+        	return num < 10 ? '0' + num.toString() : num.toString();
+        }
+
+        function validation() {
+        	console.log($scope.name)
+        	if (!$scope.name || $scope.name == undefined || $scope.name.length == 0) {
+        		alert('Cluster mentor name not filled.');
+        		return;
+        	}
+        	if (!$scope.cluster) {
+        		alert('Cluster name not selected.');	
+        		return;
+        	}
+        	if (!$scope.first_room || !$scope.second_room) {
+        		alert('Room number not selected.');
+        		return;
+        	}
+        }
+
+        validation();
+
+        if (!$scope.ignore_first_room && !$scope.ignore_first_room_left) {
+        	var response = angular.copy($scope.individual_responses[0]);
+        	$.extend(response, $scope.room_responses[0]);
+        	$.extend(response, $scope.common_area_responses);
+        	response['Room'] = $scope.cluster + '-' + formatRoom($scope.first_room) + '-1';
+        	responses_to_submit.push(response);
+        	objects_to_submit.push(new CleanlinessObject());
+        }
+
+        if (!$scope.ignore_first_room && !$scope.ignore_first_room_right) {
+        	var response = angular.copy($scope.individual_responses[1]);
+        	$.extend(response, $scope.room_responses[0]);
+        	$.extend(response, $scope.common_area_responses);
+        	response['Room'] = $scope.cluster + '-' + formatRoom($scope.first_room) + '-2';
+        	responses_to_submit.push(response);
+        	objects_to_submit.push(new CleanlinessObject());
+        }
+
+        if (!$scope.ignore_second_room && !$scope.ignore_second_room_left) {
+        	var response = angular.copy($scope.individual_responses[2]);
+        	$.extend(response, $scope.room_responses[1]);
+        	$.extend(response, $scope.common_area_responses);
+        	response['Room'] = $scope.cluster + '-' + formatRoom($scope.second_room) + '-1';
+        	responses_to_submit.push(response);
+        	objects_to_submit.push(new CleanlinessObject());
+        }
+
+        if (!$scope.ignore_second_room && !$scope.ignore_second_room_right) {
+        	var response = angular.copy($scope.individual_responses[3]);
+        	$.extend(response, $scope.room_responses[1]);
+        	$.extend(response, $scope.common_area_responses);
+        	response['Room'] = $scope.cluster + '-' + formatRoom($scope.second_room) + '-2';
+        	responses_to_submit.push(response);
+        	objects_to_submit.push(new CleanlinessObject());
+        }
+
+        $.map(responses_to_submit, function(res) {
+        	res['Cluster_mentor'] = $scope.name;
+        })
+        
+        var num_of_submitted_responses = 0;
+        
+        for (var i = 0; i < responses_to_submit.length; i++) {
+	        objects_to_submit[i].save(
+	        	responses_to_submit[i], {
+	          	success: function(data) {
+				    // Execute any logic that should take place after the object is saved.
+				    num_of_submitted_responses++;
+				    console.log(num_of_submitted_responses);
+				    if (num_of_submitted_responses == responses_to_submit.length) {
+					    alert('Responses submitted!');
+					    location.reload();
+					}
+				},
+				error: function(data, error) {
+				    // Execute any logic that should take place if the save fails.
+				    // error is a Parse.Error with an error code and description.
+				    alert('Failed to submit response: ' + error.description);
+				}
+	        });
+	    }
 	}
 }
